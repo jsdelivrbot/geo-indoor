4e103c3b392fb3a8a36a18815ba64890017ce739
jppasvit <juanpedro199travis@hotmail.com>
2017-11-03 00:02:53 +0100

    Actualización del despliegue de geoindoor a otro formato #35

--- Paquete Geoindoor/PaqueteGeoindoor/geoindoor/server_init.bat
+++ Paquete Geoindoor/PaqueteGeoindoor/geoindoor/server_init.bat
@@ -1,11 +1,21 @@
 @echo off
-echo 				       *********************
-echo 				       *    ATENTION !!!   *
-echo 				       *********************
+echo 				       **********************
+echo 				       *    ATTENTION !!!   *
+echo 				       **********************
 echo  **********************************************************************************************
 echo  * 1.- Python has to be installed to run the application                                      *
 echo  * 2.- The file named 'inicio.bat' has to be in the same path than the folder 'geoindoor'     *
-echo  * 3.- The file named 'server_init.bat' has to be in the same path than the folder 'geoindoor'*
+echo  * 3.- The file named 'server_init.bat' has to be inside the folder 'geoindoor'               *
+echo  *                                                                                            *
+echo  * BROWSER:                                                                                   *
+echo  * Default: Google Chrome                                                                     *
+echo  *                                                                                            *
+echo  * If you want to change the browser you have to execute the file 'inicio.bat', from the path *
+echo  * where 'inicio.bat' is.                                                                     *
+echo  *                                                                                            *
+echo  * Mozilla Firefox: 'start inicio.bat firefox'                                                *
+echo  * Internet Explorer: 'start inicio.bat iexplore'                                             *
+echo  * Google Chrome: 'start inicio.bat chrome'                                                   *
 echo  **********************************************************************************************
     
 python -m http.server 9000
--- Paquete Geoindoor/PaqueteGeoindoor/inicio.bat
+++ Paquete Geoindoor/PaqueteGeoindoor/inicio.bat
@@ -1,17 +1,54 @@
 @echo off
-echo 				       *********************
-echo 				       *    ATENTION !!!   *
-echo 				       *********************
+echo 				       **********************
+echo 				       *    ATTENTION !!!   *
+echo 				       **********************
 echo  **********************************************************************************************
 echo  * 1.- Python has to be installed to run the application                                      *
 echo  * 2.- The file named 'inicio.bat' has to be in the same path than the folder 'geoindoor'     *
-echo  * 3.- The file named 'server_init.bat' has to be insade the folder 'geoindoor'               *
+echo  * 3.- The file named 'server_init.bat' has to be inside the folder 'geoindoor'               *
+echo  *                                                                                            *
+echo  * BROWSER:                                                                                   *
+echo  * Default: Google Chrome                                                                     *
+echo  *                                                                                            *
+echo  * If you want to change the browser you have to execute the file 'inicio.bat', from the path *
+echo  * where 'inicio.bat' is.                                                                     *
+echo  *                                                                                            *
+echo  * Mozilla Firefox: 'start inicio.bat firefox'                                                *
+echo  * Internet Explorer: 'start inicio.bat iexplore'                                             *
+echo  * Google Chrome: 'start inicio.bat chrome'                                                   *
 echo  **********************************************************************************************
    
 echo Starting Geoindoor...
 PING localhost -n 2 >NUL
-cd .\geoindoor\
+set "my_path=%cd%"
+cd "%my_path%\geoindoor"
 start server_init.bat
-start chrome "http://localhost:9000/index.html"
+
+if "%1" == "" (
+	start chrome "http://localhost:9000/index.html"
+)
+
+if "%1" == "chrome" ( 
+	start chrome "http://localhost:9000/index.html"
+)
+
+if "%1" == "firefox" ( 
+	start firefox "http://localhost:9000/index.html"
+)
+
+if "%1" == "iexplore" ( 
+	start iexplore "http://localhost:9000/index.html"
+)
+
+if NOT "%1" == "" (
+	if NOT "%1" == "chrome" ( 
+		if NOT "%1" == "firefox" ( 
+			if NOT "%1" == "iexplore" ( 
+				start chrome "http://localhost:9000/index.html"
+			)
+		)
+	)
+)
+
 
 pause
