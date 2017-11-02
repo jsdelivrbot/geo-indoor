84182f0a83f5b32134c6f40178a9b1719f5776af
jppasvit <juanpedro199travis@hotmail.com>
2017-11-02 22:38:23 +0100

    ctualización del despliegue de geoindoor #35

--- Paquete Geoindoor/PaqueteGeoindoor/geoindoor/server_init.bat
+++ Paquete Geoindoor/PaqueteGeoindoor/geoindoor/server_init.bat
@@ -5,13 +5,7 @@ echo 				       *********************
 echo  **********************************************************************************************
 echo  * 1.- Python has to be installed to run the application                                      *
 echo  * 2.- The file named 'inicio.bat' has to be in the same path than the folder 'geoindoor'     *
-echo  * 3.- The file named 'server_init.bat' has to be insade the folder 'geoindoor'               *
+echo  * 3.- The file named 'server_init.bat' has to be in the same path than the folder 'geoindoor'*
 echo  **********************************************************************************************
-   
-echo Starting Geoindoor...
-PING localhost -n 2 >NUL
-cd .\geoindoor\
-start server_init.bat
-start chrome "http://localhost:9000/index.html"
-
-pause
+    
+python -m http.server 9000
