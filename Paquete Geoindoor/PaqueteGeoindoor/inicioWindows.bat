8d375ec0e4aa628e46667b96c72f5e2c3a458975
jppasvit <juanpedro199travis@hotmail.com>
2018-01-08 22:53:18 +0100

    Actualización de paquete Geoindoor - Se actualizan los textos de los lanzadores - Se elimina redirección a ningún lado

--- Paquete Geoindoor/PaqueteGeoindoor/inicioWindows.bat
+++ Paquete Geoindoor/PaqueteGeoindoor/inicioWindows.bat
@@ -2,21 +2,21 @@
 echo 				       **********************
 echo 				       *    ATTENTION !!!   *
 echo 				       **********************
-echo  **********************************************************************************************
-echo  * 1.- Python has to be installed to run the application                                      *
-echo  * 2.- The file named 'inicio.bat' has to be in the same path than the folder 'geoindoor'     *
-echo  * 3.- The file named 'server_init.bat' has to be inside the folder 'geoindoor'               *
-echo  *                                                                                            *
-echo  * BROWSER:                                                                                   *
-echo  * Default: Google Chrome                                                                     *
-echo  *                                                                                            *
-echo  * If you want to change the browser you have to execute the file 'inicio.bat', from the path *
-echo  * where 'inicio.bat' is.                                                                     *
-echo  *                                                                                            *
-echo  * Mozilla Firefox: 'start inicio.bat firefox'                                                *
-echo  * Internet Explorer: 'start inicio.bat iexplore'                                             *
-echo  * Google Chrome: 'start inicio.bat chrome'                                                   *
-echo  **********************************************************************************************
+echo  ************************************************************************************************
+echo  * 1.- Python has to be installed to run the application                                        *
+echo  * 2.- The file named 'inicioWindows.bat' has to be in the same path than the folder 'geoindoor'*
+echo  * 3.- The file named 'server_init.bat' has to be inside the folder 'geoindoor'                 *
+echo  *                                                                                              *
+echo  * BROWSER:                                                                                     *
+echo  * Default: Google Chrome                                                                       *
+echo  *                                                                                              *
+echo  * If you want to change the browser you have to execute the file 'inicioWindows.bat', from the *
+echo  * path where 'inicioWindows.bat' is.                                                           *
+echo  *                                                                                              *
+echo  * Mozilla Firefox: 'start inicioWindows.bat firefox'                                           *
+echo  * Internet Explorer: 'start inicioWindows.bat iexplore'                                        *
+echo  * Google Chrome: 'start inicioWindows.bat chrome'                                              *
+echo  ************************************************************************************************
    
 echo Starting Geoindoor...
 PING localhost -n 2 >NUL
